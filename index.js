63eb6e7c81827dd8b5a3ad050886662454a7cd06
Senpiie <lamabishal1542@gmail.com>
2024-03-15 12:45:20 +0545

    haha

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const personName = 'bishal';
+const age = 90;
+const rating = 90.99;
+const isShow = true;
+const val = null;
+
+console.log(gee);
+console.log(val);
+console.log(personName);
+console.log(rating);
